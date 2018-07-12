b4e6b110ad90ac073204110592ce308ac27398f8
Rohan Paul <rohanpaul2@gmail.com>
2018-07-13 01:53:29 +0530

    completeting routes for ./routes/book.js

---
+++ routes/book.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Book = require('../models/Book.js')
+
+// Get all books
+router.get('/', (req, res, next) => {
+  Book.find((err, products) => {
+    if (err) return next(err);
+    res.json(products)
+  });
+});
+
+// Get single book by Id
+router.get('/:id', (req, res, next) => {
+  Book.findById(req.params.id, (err, post) => {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// Save Book
+router.post('/', (req, res, next) => {
+  Book.create(req.body, (err, post) => {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// Update Book
+router.put('/:id', (req, res, next) => {
+  Book.findByIdAndUpdate(req.params.id, req.body, (err, post) => {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// Delete Book
+router.delete('/:id', (req, res, next) => {
+  Book.findByIdAndRemove(req.params.id, req.body, (err, post) => {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
